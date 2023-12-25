22d87331a53700fdca7735ee11f045362f7df38a
hoanganh5350 <hoanganh2000vip1@example.com>
2023-12-26 02:17:24 +0700

    update

---
+++ frontstore/pages/detail/Detail.tsx
@@ -0,0 +1,20 @@
+import React, { Key, useEffect, useState } from "react";
+import styles from "./Detail.module.scss";
+import { Button, SlickHeader } from "../../component";
+
+const Detail = () => {
+
+  
+
+  //function to render
+
+  
+
+  //Main render
+  return (
+    <div className={[styles.Detail].join(" ")}>
+      
+    </div>
+  );
+};
+export default Detail;
